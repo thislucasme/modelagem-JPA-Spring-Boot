99230b9158f77d2a9fb87c1f409437fe6c3ca126
Lucas Silva <lucassilvaee1245@gmail.com>
2021-06-22 15:36:01 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/thislucasme/CursomcApplication.java
+++ src/main/java/com/thislucasme/CursomcApplication.java
@@ -1,6 +1,7 @@
 package com.thislucasme;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -8,13 +9,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.thislucasme.domain.Categoria;
+import com.thislucasme.domain.Produto;
 import com.thislucasme.repositories.CategoriaRepository;
+import com.thislucasme.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -26,7 +32,25 @@ public class CursomcApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto p1 =new Produto(null, "Celular", 1320.98);
+		Produto p2 =new Produto(null, "MacBook Pro", 6400.48);
+		Produto p3 =new Produto(null, "Miband", 310.53);
+		
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 
 }
--- src/main/java/com/thislucasme/repositories/ProdutoRepository.java
+++ src/main/java/com/thislucasme/repositories/ProdutoRepository.java
@@ -2,9 +2,10 @@ package com.thislucasme.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.thislucasme.domain.Categoria;;
+import com.thislucasme.domain.Categoria;
+import com.thislucasme.domain.Produto;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
 	
 }
